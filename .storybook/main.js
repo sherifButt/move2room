1a8e843ff2cf5c0a5ecb0bc42d7522e03b3af74e
sherif.butt@gmail.com <sherif.butt@gmail.com>
2022-04-13 13:21:58 +0100

    moved stories from pages folder to fix error: component witout default react export

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
    stories: [
       // Paths to the story files
-      "../pages/stories/*.stories.mdx",
-      "../pages/stories/*.stories.js",
+      "../stories/*.stories.mdx",
+      "../stories/*.stories.js",
    ],
    addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 };
